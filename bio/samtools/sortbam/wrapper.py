0f1f171dd29196ec866623115650637732c7cb49
riley-seoyoon <sypark02178@gmail.com>
2022-11-08 16:57:36 +0900

    edit sortbam

--- bio/samtools/sortbam/wrapper.py
+++ bio/samtools/sortbam/wrapper.py
@@ -18,14 +18,14 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 with tempfile.TemporaryDirectory() as tmpdir:
     tmp_prefix = Path(tmpdir) / "samtools_fastq.sort_"
 
-    shell(
-        "cat <( samtools view -H {inputbam} )"
-        " <( samtools view -@ 12 {inputbam}"
-        " | awk '{printf "%s", $0 " "; getline; print}'"
-        " | sort -S 50G -T {tmp_prefix}"
-        " | tr ' ' '\n' )"
-        " | samtools view"
-        " -@ 12 -bS -"
-        " > {outbam}"
-        " {log}"
-        )
+shell(
+    "cat <( samtools view -H {inputbam} )"
+    " <( samtools view -@ 12 {inputbam}"
+    " | awk '{printf '%s', $0 ' '; getline; print}'"
+    " | sort -S 50G -T {tmp_prefix}"
+    " | tr ' ' '\n' )"
+    " | samtools view"
+    " -@ 12 -bS -"
+    " > {outbam}"
+    " {log}"
+)
