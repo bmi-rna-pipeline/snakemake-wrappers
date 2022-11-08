f5e38cd66c69777c0516bd5fe2bb82ad379b5370
riley-seoyoon <sypark02178@gmail.com>
2022-11-08 17:11:38 +0900

    esit sortbam

--- bio/samtools/sortbam/wrapper.py
+++ bio/samtools/sortbam/wrapper.py
@@ -14,7 +14,7 @@ outbam = snakemake.output.sortedout
 
 samtools_opts = get_samtools_opts(snakemake)
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-awk = '| awk''{printf "%s", $0 ""; getline; print}'
+awk = "| awk '{}'".format('{printf "%s", $0 ""; getline; print}')
 
 with tempfile.TemporaryDirectory() as tmpdir:
     tmp_prefix = Path(tmpdir) / "samtools_fastq.sort_"
