ec759f0bbccdba5a8329b9df9b1fd08c4e4dcdb0
박서윤 <104350185+riley-seoyoon@users.noreply.github.com>
2022-11-08 17:42:55 +0900

    Update wrapper.py

--- bio/samtools/sortbam/wrapper.py
+++ bio/samtools/sortbam/wrapper.py
@@ -15,18 +15,20 @@ outbam = snakemake.output.sortedout
 samtools_opts = get_samtools_opts(snakemake)
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+awk = '{printf "%s", $0 ""; getline; print}'
+
 with tempfile.TemporaryDirectory() as tmpdir:
     tmp_prefix = Path(tmpdir) / "samtools_fastq.sort_"
 
-shell(
-    """
-    cat <( samtools view -H {inputbam} ) \
-    <( samtools view -@ 12 {inputbam} \
-     | awk '{{printf "%s", $0 ""; getline; print}}' \
-     | sort -S 50G -T {tmp_prefix} \
-     | tr ' ' '\n' ) \
-     | samtools view \
-     -@ 12 -bS -> {outbam} \
-     {log}
-     """
-)
+    shell(
+        """
+        cat <( samtools view -H {inputbam} ) \
+        <( samtools view -@ 12 {inputbam} \
+         | awk '{awk}' \
+         | sort -S 50G -T {tmp_prefix} \
+         | tr ' ' '\n' ) \
+         | samtools view \
+         -@ 12 -bS -> {outbam} \
+         {log}
+         """
+    )
