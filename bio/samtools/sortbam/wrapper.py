383f485278cb7e6e2966ec504f7febbb4c67fc67
riley-seoyoon <sypark02178@gmail.com>
2022-11-08 16:56:11 +0900

    edit sortbam

--- bio/samtools/sortbam/wrapper.py
+++ bio/samtools/sortbam/wrapper.py
@@ -19,12 +19,13 @@ with tempfile.TemporaryDirectory() as tmpdir:
     tmp_prefix = Path(tmpdir) / "samtools_fastq.sort_"
 
     shell(
-        "cat <(samtools view -H {inputbam}) \
-        <(samtools view -@ {snakemake.threads} {inputbam} \
-        | awk '{printf "%s", $0 ""; getline; print}' \
-        | sort -S 50G -T {tmp_prefix} \
-        | tr \n') \
-        | samtools view \
-        -@ {snakemake.threads} -bS -> {outbam}"
+        "cat <( samtools view -H {inputbam} )"
+        " <( samtools view -@ 12 {inputbam}"
+        " | awk '{printf "%s", $0 " "; getline; print}'"
+        " | sort -S 50G -T {tmp_prefix}"
+        " | tr ' ' '\n' )"
+        " | samtools view"
+        " -@ 12 -bS -"
+        " > {outbam}"
         " {log}"
         )
